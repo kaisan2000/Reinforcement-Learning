8f69fa2c9fccc7f8818667dcd075d8a091d9274a
kaisan2000 <ko449@snu.edu.in>
2024-09-26 13:24:06 +0530

    Add files via upload

--- constantalpha.py
+++ constantalpha.py
@@ -1,7 +1,8 @@
 import numpy as np
 import maze
-import matplotlib.pyplot as plt
+
 from random import randint
+import time
 env = maze.Maze()
 env.reset()
 env.render()
@@ -9,25 +10,23 @@ env.render()
 print(f"Observation space shape: {env.observation_space.nvec}")
 print(f"Number of actions: {env.action_space.n}")
 Q = {}
-
-
-action_values = np.zeros(shape=(5,5,4))
+a_v = np.zeros(shape=(5,5,4))
 
 for i in range(5):
     for j in range(5):
         state = (i,j)
         for action in range(4):
-            Q[state,action] = np.random.rand(0,1)
+            qsa =  Q[tuple(state),action] = np.random.rand()
 
 
 
-qsa = [0.3,2,1,4]
+
 
 def policy(state,epsilon=0.1):
     if np.random.random() < epsilon:
         return np.random.randint(4)
     else:
-        return np.argmax(qsa)
+        return action == np.argmax(qsa)
         
 
         
@@ -36,24 +35,43 @@ epsilon = 0.1
 num_episodes = 20
 alpha = 0.1 
 
-def on_policy_mc_control(policy,action_values,episodes,gamma,epsilon,alpha):
+def on_policy_mc_control(policy,a_v,episodes,gamma=0.99,epsilon=0.1,alpha=0.1):
     for episode in range(num_episodes):
         state = env.reset()
         done = False
         transitions = []
+        step_count = 0
+
+        print(f"Episode{episode + 1}/{num_episodes}")
 
         while not done:
             action = policy(state,epsilon)
-            next_state,reward,done = env.step(action)
-            transitions.append([state,action,reward])
+            next_state,reward,done,_ = env.step(action)
+            transitions.append([tuple(state),action,reward])
+
+            env.render()
+            time.sleep(0.01)
+
             state = next_state
+            step_count +=1
 
             G = 0
             for state_t,action_t,reward_t in reversed(transitions):
                 G = reward_t + gamma*G
 
-                qsa = action_values[state_t][action_t]
-                action_values[state_t][action_t] += alpha*(G-qsa)
+                qsa = a_v[tuple(state_t)][action_t]
+                a_v[tuple(state_t)][action_t] += alpha*(G-qsa)
+
+        print(f"Episode{episode + 1} finished after {step_count} steps.\n")
+
+
+on_policy_mc_control(policy,a_v,episodes = 20)
+
+
+
+print("Learned Q-values:")
+print(a_v)
+                       
 
 
 
